801f2c312784bc97d21768bfb675f4bcdf658c77
bryancasler <bryan.casler@gmail.com>
2022-06-30 20:22:17 -0400

    Rebuild

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, June 24, 2022 @ 22:44:33 ET
+ *  Date: Thursday, June 30, 2022 @ 20:17:43 ET
  *  By: bryancasler
  *  ENGrid styles: v0.12.12
  *  ENGrid scripts: v0.12.17
@@ -11231,7 +11231,7 @@ class ProcessingFees {
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-common/dist/app.js
 
 
-class app_App extends engrid_ENGrid {
+class App extends engrid_ENGrid {
     constructor(options) {
         super();
         // Events
@@ -11287,9 +11287,9 @@ class app_App extends engrid_ENGrid {
             // Add Options to window
             window.EngridOptions = this.options;
         }
-        if (this.options.Debug || app_App.getUrlParameter("debug") == "true")
+        if (this.options.Debug || App.getUrlParameter("debug") == "true")
             // Enable debug if available is the first thing
-            app_App.setBodyData("debug", "");
+            App.setBodyData("debug", "");
         // TODO: Abstract everything to the App class so we can remove custom-methods
         inputPlaceholder();
         preventAutocomplete();
@@ -11466,7 +11466,7 @@ class app_App extends engrid_ENGrid {
     setDataAttributes() {
         // Add the Page Type as a Data Attribute on the video
         if (engrid_ENGrid.checkNested(window, "pageJson", "pageType")) {
-            app_App.setBodyData("page-type", window.pageJson.pageType);
+            App.setBodyData("page-type", window.pageJson.pageType);
             this.logger.log("Page Type: " + window.pageJson.pageType);
         }
         else {
@@ -11474,86 +11474,86 @@ class app_App extends engrid_ENGrid {
         }
         // Add a body banner data attribute if the banner contains no image or video
         if (!document.querySelector(".body-banner img, .body-banner video")) {
-            app_App.setBodyData("body-banner", "empty");
+            App.setBodyData("body-banner", "empty");
         }
         // Add a page-alert data attribute if it is empty
         if (!document.querySelector(".page-alert *")) {
-            app_App.setBodyData("no-page-alert", "");
+            App.setBodyData("no-page-alert", "");
         }
         // Add a content-header data attribute if it is empty
         if (!document.querySelector(".content-header *")) {
-            app_App.setBodyData("no-content-header", "");
+            App.setBodyData("no-content-header", "");
         }
         // Add a body-headerOutside data attribute if it is empty
         if (!document.querySelector(".body-headerOutside *")) {
-            app_App.setBodyData("no-body-headerOutside", "");
+            App.setBodyData("no-body-headerOutside", "");
         }
         // Add a body-header data attribute if it is empty
         if (!document.querySelector(".body-header *")) {
-            app_App.setBodyData("no-body-header", "");
+            App.setBodyData("no-body-header", "");
         }
         // Add a body-title data attribute if it is empty
         if (!document.querySelector(".body-title *")) {
-            app_App.setBodyData("no-body-title", "");
+            App.setBodyData("no-body-title", "");
         }
         // Add a body-banner data attribute if it is empty
         if (!document.querySelector(".body-banner *")) {
-            app_App.setBodyData("no-body-banner", "");
+            App.setBodyData("no-body-banner", "");
         }
         // Add a body-bannerOverlay data attribute if it is empty
         if (!document.querySelector(".body-bannerOverlay *")) {
-            app_App.setBodyData("no-body-bannerOverlay", "");
+            App.setBodyData("no-body-bannerOverlay", "");
         }
         // Add a body-top data attribute if it is empty
         if (!document.querySelector(".body-top *")) {
-            app_App.setBodyData("no-body-top", "");
+            App.setBodyData("no-body-top", "");
         }
         // Add a body-main data attribute if it is empty
         if (!document.querySelector(".body-main *")) {
-            app_App.setBodyData("no-body-main", "");
+            App.setBodyData("no-body-main", "");
         }
         // Add a body-bottom data attribute if it is empty
         if (!document.querySelector(".body-bottom *")) {
-            app_App.setBodyData("no-body-bottom", "");
+            App.setBodyData("no-body-bottom", "");
         }
         // Add a body-footer data attribute if it is empty
         if (!document.querySelector(".body-footer *")) {
-            app_App.setBodyData("no-body-footer", "");
+            App.setBodyData("no-body-footer", "");
         }
         // Add a body-footerOutside data attribute if it is empty
         if (!document.querySelector(".body-footerOutside *")) {
-            app_App.setBodyData("no-body-footerOutside", "");
+            App.setBodyData("no-body-footerOutside", "");
         }
         // Add a content-footerSpacer data attribute if it is empty
         if (!document.querySelector(".content-footerSpacer *")) {
-            app_App.setBodyData("no-content-footerSpacer", "");
+            App.setBodyData("no-content-footerSpacer", "");
         }
         // Add a content-preFooter data attribute if it is empty
         if (!document.querySelector(".content-preFooter *")) {
-            app_App.setBodyData("no-content-preFooter", "");
+            App.setBodyData("no-content-preFooter", "");
         }
         // Add a content-footer data attribute if it is empty
         if (!document.querySelector(".content-footer *")) {
-            app_App.setBodyData("no-content-footer", "");
+            App.setBodyData("no-content-footer", "");
         }
         // Add a page-backgroundImage banner data attribute if the page background image contains no image or video
         if (!document.querySelector(".page-backgroundImage img, .page-backgroundImage video")) {
-            app_App.setBodyData("no-page-backgroundImage", "");
+            App.setBodyData("no-page-backgroundImage", "");
         }
         // Add a page-backgroundImageOverlay data attribute if it is empty
         if (!document.querySelector(".page-backgroundImageOverlay *")) {
-            app_App.setBodyData("no-page-backgroundImageOverlay", "");
+            App.setBodyData("no-page-backgroundImageOverlay", "");
         }
         // Add a page-customCode data attribute if it is empty
         if (!document.querySelector(".page-customCode *")) {
-            app_App.setBodyData("no-page-customCode", "");
+            App.setBodyData("no-page-customCode", "");
         }
         // Add a country data attribute
         const countrySelect = document.querySelector("#en__field_supporter_country");
         if (countrySelect) {
-            app_App.setBodyData("country", countrySelect.value);
+            App.setBodyData("country", countrySelect.value);
             countrySelect.addEventListener("change", () => {
-                app_App.setBodyData("country", countrySelect.value);
+                App.setBodyData("country", countrySelect.value);
             });
         }
     }
@@ -16164,8 +16164,6 @@ const AppVersion = "0.12.17";
 ;// CONCATENATED MODULE: ./src/scripts/main.js
 const customScript = function () {
   console.log("ENGrid client scripts are executing"); // Add your client scripts here
-
-  App.setBodydata("client-js-loading", "finished");
 };
 ;// CONCATENATED MODULE: ./src/index.ts
  // Uses ENGrid via NPM
@@ -16184,11 +16182,11 @@ const options = {
   SkipToMainContentLink: true,
   SrcDefer: true,
   ProgressBar: true,
-  Debug: app_App.getUrlParameter("debug") == "true" ? true : false,
+  Debug: App.getUrlParameter("debug") == "true" ? true : false,
   onLoad: () => customScript(),
   onResize: () => console.log("Starter Theme Window Resized")
 };
-new app_App(options);
+new App(options);
 })();
 
 /******/ })()
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,4 @@
 export const customScript = function () {
   console.log("ENGrid client scripts are executing");
   // Add your client scripts here
-
-  App.setBodydata("client-js-loading", "finished");
 };
