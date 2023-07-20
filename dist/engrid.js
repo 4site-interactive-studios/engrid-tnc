78afad2256fb9fd51130bf2699db64d93fe7b22c
Michael Thomas <michaelthomas372@gmail.com>
2023-07-20 18:50:56 +0200

    base for page template

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, July 19, 2023 @ 11:31:21 ET
+ *  Date: Thursday, July 20, 2023 @ 12:50:20 ET
  *  By: michael
  *  ENGrid styles: v0.13.0
  *  ENGrid scripts: v0.13.5
@@ -7863,6 +7863,20 @@ const TranslateOptionsDefaults = {
   NL: nlTranslation,
   NLD: nlTranslation
 };
+;// CONCATENATED MODULE: ../engrid-scripts/packages/common/dist/interfaces/exit-intent-options.js
+const ExitIntentOptionsDefaults = {
+  enabled: false,
+  title: "We are sad that you are leaving",
+  text: "Would you mind telling us why you are leaving this page?",
+  buttonText: "Send us your comments",
+  buttonLink: "https://www.4sitestudios.com/",
+  cookieName: "engrid-exit-intent-lightbox",
+  cookieDuration: 30,
+  triggers: {
+    visibilityState: true,
+    mousePosition: true
+  }
+};
 ;// CONCATENATED MODULE: ../engrid-scripts/packages/common/dist/loader.js
 // Ref: https://app.getguru.com/card/iMgx968T/ENgrid-Loader
 
@@ -9333,7 +9347,9 @@ class App extends engrid_ENGrid {
     if (this.options.Plaid) new Plaid(); // Give By Select
 
     new GiveBySelect();
-    this.setDataAttributes();
+    this.setDataAttributes(); //Exit Intent Lightbox
+
+    new ExitIntentLightbox();
     new UrlParamsToBodyAttrs(); //Debug panel
 
     if (this.options.Debug || window.sessionStorage.hasOwnProperty(DebugPanel.debugSessionStorageKey)) {
@@ -17317,10 +17333,154 @@ class UrlParamsToBodyAttrs {
   }
 
 }
+;// CONCATENATED MODULE: ../engrid-scripts/packages/common/dist/exit-intent-lightbox.js
+
+
+class ExitIntentLightbox {
+  constructor() {
+    this.opened = false;
+    this.dataLayer = window.dataLayer || [];
+    this.logger = new EngridLogger("ExitIntentLightbox", "yellow", "black", "🚪");
+    let options = "EngridExitIntent" in window ? window.EngridExitIntent : {};
+    this.options = Object.assign(Object.assign({}, ExitIntentOptionsDefaults), options);
+
+    if (!this.options.enabled) {
+      this.logger.log("Not enabled");
+      return;
+    }
+
+    if (get(this.options.cookieName)) {
+      this.logger.log("Not showing - cookie found.");
+      return;
+    }
+
+    const activeTriggers = Object.keys(this.options.triggers).filter(t => this.options.triggers[t]).join(", ");
+    this.logger.log("Enabled, waiting for trigger. Active triggers: " + activeTriggers);
+    this.watchForTriggers();
+  }
+
+  watchForTriggers() {
+    if (this.options.triggers.mousePosition) {
+      this.watchMouse();
+    }
+
+    if (this.options.triggers.visibilityState) {
+      this.watchDocumentVisibility();
+    }
+  }
+
+  watchMouse() {
+    document.addEventListener("mouseout", e => {
+      // If this is an autocomplete element.
+      if (e.target.tagName.toLowerCase() == "input") return; // Get the current viewport width.
+
+      const vpWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0); // If the current mouse X position is within 50px of the right edge
+      // of the viewport, return.
+
+      if (e.clientX >= vpWidth - 50) return; // If the current mouse Y position is not within 50px of the top
+      // edge of the viewport, return.
+
+      if (e.clientY >= 50) return; // Reliable, works on mouse exiting window and
+      // user switching active program
+
+      const from = e.relatedTarget;
+
+      if (!from) {
+        this.logger.log("Triggered by mouse position");
+        this.open();
+      }
+    });
+  }
+
+  watchDocumentVisibility() {
+    const visibilityListener = () => {
+      if (document.visibilityState === "hidden") {
+        this.logger.log("Triggered by visibilityState is hidden");
+        this.open();
+        document.removeEventListener("visibilitychange", visibilityListener);
+      }
+    };
+
+    document.addEventListener("visibilitychange", visibilityListener);
+  }
+
+  open() {
+    var _a, _b, _c;
+
+    if (this.opened) return;
+    engrid_ENGrid.setBodyData("exit-intent-lightbox", "open");
+    set(this.options.cookieName, "1", {
+      expires: this.options.cookieDuration
+    });
+    document.body.insertAdjacentHTML("beforeend", `
+        <div class="ExitIntent">
+          <div class="ExitIntent__overlay">
+            <div class="ExitIntent__container">
+              <div class="ExitIntent__close">X</div>
+              <div class="ExitIntent__body">
+                <h2>${this.options.title}</h2>
+                <p>${this.options.text}</p>
+                <button type="button" class="ExitIntent__button">
+                  ${this.options.buttonText}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `);
+    this.opened = true;
+    this.dataLayer.push({
+      event: "exit_intent_lightbox_shown"
+    });
+    (_a = document.querySelector(".ExitIntent__close")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+      this.dataLayer.push({
+        event: "exit_intent_lightbox_closed"
+      });
+      this.close();
+    });
+    (_b = document.querySelector(".ExitIntent__overlay")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", event => {
+      if (event.target === event.currentTarget) {
+        this.dataLayer.push({
+          event: "exit_intent_lightbox_closed"
+        });
+        this.close();
+      }
+    });
+    (_c = document.querySelector(".ExitIntent__button")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
+      this.dataLayer.push({
+        event: "exit_intent_lightbox_cta_clicked"
+      });
+      this.close();
+      const target = this.options.buttonLink;
+
+      if (target.startsWith(".") || target.startsWith("#")) {
+        const targetEl = document.querySelector(target);
+
+        if (targetEl) {
+          targetEl.scrollIntoView({
+            behavior: "smooth"
+          });
+        }
+      } else {
+        window.open(target, "_blank");
+      }
+    });
+  }
+
+  close() {
+    var _a;
+
+    (_a = document.querySelector(".ExitIntent")) === null || _a === void 0 ? void 0 : _a.remove();
+    engrid_ENGrid.setBodyData("exit-intent-lightbox", "closed");
+  }
+
+}
 ;// CONCATENATED MODULE: ../engrid-scripts/packages/common/dist/version.js
-const AppVersion = "0.14.11";
+const AppVersion = "0.14.12";
 ;// CONCATENATED MODULE: ../engrid-scripts/packages/common/dist/index.js
  // Runs first so it can change the DOM markup before any markup dependent code fires
+
+
 
 
 
