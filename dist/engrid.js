d9f082e970c482f872635239ff1be581b80d1d2c
Michael Thomas <michaelthomas372@gmail.com>
2023-12-07 12:05:57 +0100

    fix banner image sizing with variable content in safari/firefox

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, November 27, 2023 @ 12:54:32 ET
+ *  Date: Thursday, December 7, 2023 @ 05:56:27 ET
  *  By: michael
  *  ENGrid styles: v0.15.12
  *  ENGrid scripts: v0.15.15
