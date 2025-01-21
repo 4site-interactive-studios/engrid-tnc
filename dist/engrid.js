21e7a774f56d700620b9436061749944818102a3
Michael Thomas <michaelthomas372@gmail.com>
2025-01-21 14:34:16 +0100

    hero banner layout + accoridon

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, January 21, 2025 @ 07:00:19 ET
+ *  Date: Tuesday, January 21, 2025 @ 08:33:49 ET
  *  By: michael
  *  ENGrid styles: v0.19.16
  *  ENGrid scripts: v0.19.19
