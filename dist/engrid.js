a3be11e064680a9c8c93ce23a72904017b9eea58
Fernando Santos <fern@ndo.io>
2025-06-11 22:44:46 -0400

    Style Changes for GDCP on Multistep

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, June 6, 2025 @ 15:12:50 ET
+ *  Date: Wednesday, June 11, 2025 @ 22:00:05 ET
  *  By: fernando
  *  ENGrid styles: v0.22.4
  *  ENGrid scripts: v0.22.6
