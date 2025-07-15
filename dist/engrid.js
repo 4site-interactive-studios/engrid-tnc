d4bb735887783236e4ee98be0275d7baaac61b31
Michael Thomas <9821783+MichaelT372@users.noreply.github.com>
2025-07-15 13:37:43 +0200

    July 2025 ENgrid update: v0.22.6 to v0.22.7

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,10 +17,10 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, July 15, 2025 @ 07:12:03 ET
+ *  Date: Tuesday, July 15, 2025 @ 07:29:25 ET
  *  By: michael
  *  ENGrid styles: v0.22.4
- *  ENGrid scripts: v0.22.6
+ *  ENGrid scripts: v0.22.7
  *
  *  Created by 4Site Studios
  *  Come work with us or join our team, we would love to hear from you
@@ -22311,7 +22311,7 @@ class CheckboxLabel {
             var _a;
             const labelText = (_a = checkboxLabel.textContent) === null || _a === void 0 ? void 0 : _a.trim();
             const checkboxContainer = checkboxLabel.nextElementSibling;
-            const checkboxLabelElement = checkboxContainer.querySelector("label");
+            const checkboxLabelElement = checkboxContainer.querySelector("label:last-child");
             if (!checkboxLabelElement || !labelText)
                 return;
             checkboxLabelElement.textContent = labelText;
@@ -22835,7 +22835,7 @@ class FrequencyUpsell {
 }
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-scripts/dist/version.js
-const AppVersion = "0.22.6";
+const AppVersion = "0.22.7";
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-scripts/dist/index.js
  // Runs first so it can change the DOM markup before any markup dependent code fires
