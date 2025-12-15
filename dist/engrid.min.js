431308a1d022d4bdcdbb078a716337a36c229a38
cawecoy91 <cawecoy@gmail.com>
2025-12-15 18:15:03 -0300

    One column lightbox: set #engrid max-width only if embedded

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, December 15, 2025 @ 16:04:48 ET
+ *  Date: Monday, December 15, 2025 @ 16:14:01 ET
  *  By: cawe
  *  ENGrid styles: v0.23.0
  *  ENGrid scripts: v0.23.2
