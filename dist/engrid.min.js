c8a7789371f68f9a5c079d621b6798c6fb51cc66
cawecoy91 <cawecoy@gmail.com>
2025-12-31 20:21:12 -0300

    One column lightbox: fix body banner height on thankyou page (wrong css selector)

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, December 31, 2025 @ 18:17:18 ET
+ *  Date: Wednesday, December 31, 2025 @ 18:20:54 ET
  *  By: cawe
  *  ENGrid styles: v0.23.0
  *  ENGrid scripts: v0.23.2
