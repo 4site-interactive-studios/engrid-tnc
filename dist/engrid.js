8354cdeaacb446068d3d12ecc8555f095998248a
Michael Thomas <9821783+MichaelT372@users.noreply.github.com>
2026-07-13 13:31:36 +0200

    browserlist update

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, July 13, 2026 @ 07:24:13 ET
+ *  Date: Monday, July 13, 2026 @ 07:31:12 ET
  *  By: michael
  *  ENGrid styles: v0.25.11
  *  ENGrid scripts: v0.25.11
