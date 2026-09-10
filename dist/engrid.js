a590344d70bd161b2c60a573bc1ad28a74f5b46d
Michael Thomas <9821783+MichaelT372@users.noreply.github.com>
2026-09-10 17:33:23 +0200

    add checkbox 3 design and 1 and 2 enhancements

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, September 10, 2026 @ 21:22:08 ET
+ *  Date: Thursday, September 10, 2026 @ 08:12:54 ET
  *  By: michael
  *  ENGrid styles: v0.28.3
  *  ENGrid scripts: v0.28.5
