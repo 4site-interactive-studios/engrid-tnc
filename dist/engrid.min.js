bcb4c933aed527a4cf1f5fbe97f8e0fbe0b7dc56
Michael Thomas <9821783+MichaelT372@users.noreply.github.com>
2026-09-16 14:28:14 +0200

    add mobile designs and animation for sprout icon

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, September 14, 2026 @ 13:26:08 ET
+ *  Date: Wednesday, September 16, 2026 @ 08:27:43 ET
  *  By: michael
  *  ENGrid styles: v0.28.3
  *  ENGrid scripts: v0.28.5
